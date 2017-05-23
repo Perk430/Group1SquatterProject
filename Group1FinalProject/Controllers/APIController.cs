f9e23d98a6db975b6149e74fe4deb0712c9213f5
tdrier1 <tdrier1@gmail.com>
2017-05-23 14:35:13 -0400

    Added trycatch to Webrequest

--- Group1FinalProject/Controllers/APIController.cs
+++ Group1FinalProject/Controllers/APIController.cs
@@ -32,32 +32,22 @@ namespace Group1FinalProject.Controllers
             //calling API with formatted inputs
             HttpWebRequest request = WebRequest.CreateHttp($"https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1={Input.Address1}&address2={Input.Address2}");
 
-            //request.ContentType = "application / json";
-
             request.Accept = "application/xml";
 
             request.Headers.Add("APIKey", "0ad6a63ba8d89e1de14de3893e417889");
 
             //request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader rd = new StreamReader(response.GetResponseStream());
 
-            StreamReader rd = new StreamReader(response.GetResponseStream());
+                string data = rd.ReadToEnd();
 
-            string data = rd.ReadToEnd();
+                XmlDocument XMLdataTest = new XmlDocument();
 
-            //ViewBag.Message = data;
-
-            //JObject TestData = JObject.Parse(data);
-
-
-            //ViewBag.Message = TestData["response"]["result"]["package"]["item"];
-
-            //ViewBag.Time = WeatherData["time"]["startPeriodName"];
-
-            XmlDocument XMLdataTest = new XmlDocument();
-
-            XMLdataTest.LoadXml(data);
+                XMLdataTest.LoadXml(data);
 
             try
             {
@@ -95,6 +85,11 @@ namespace Group1FinalProject.Controllers
             catch
             {
                 ViewBag.propsubtype = "No info";
+            }
+            }
+            catch
+            {
+                ViewBag.Message = "Not a house!";
             }
 
             return View("../Home/APIView");
