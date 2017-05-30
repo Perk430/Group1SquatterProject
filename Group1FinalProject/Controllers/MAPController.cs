2d52f0ce93ae3fb9b02f3c408f2364e20734ee11
tdrier1 <tdrier1@gmail.com>
2017-05-30 11:31:28 -0400

    fixed redirect error

--- Group1FinalProject/Controllers/MAPController.cs
+++ Group1FinalProject/Controllers/MAPController.cs
@@ -46,8 +46,8 @@ namespace Group1FinalProject.Controllers
 
             TempData["reported"] = reported;
 
-            return View();
-            //return RedirectToAction("ReportView", "Report");
+            //return View();
+            return RedirectToAction("Redirect", "Map");
         }
 
  
